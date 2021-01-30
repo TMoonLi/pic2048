4080f208165793049eaade2df30605d775519f7b
zhangqi <epicqi@163.com>
2021-01-30 17:16:52 +0800

    mobile action

--- js/main2048.js
+++ js/main2048.js
@@ -219,7 +219,10 @@ function generateOneNumber(){
  	}
  });
 
-
+ document.body.addEventListener('touchmove', function (e) {
+	e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
+  }, {passive: false});
+  
 //document.addEventListener('touchstart',function(event){
 //	//touches.event
 //	event.preventDefault();
